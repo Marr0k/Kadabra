ffa68e2566290704ebae9d45ad9c30d44621faeb
stobsjeeve <rishabhshetty137@gmail.com>
2025-06-30 21:17:59 +1000

    Price market
    Implemented keywords, set information, mana cost and pricing options for cards

--- mtg-site/pages/cards/[name].js
+++ mtg-site/pages/cards/[name].js
@@ -16,9 +16,20 @@ export default function CardPage({ card }) {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>{card.name}</h1>
-      <img src={card.image_uris?.normal} alt={card.name} />
-      <p>{card.oracle_text}</p>
-      <p>Price (USD): ${card.prices.usd}</p>
+        <img src={card.image_uris?.normal} alt={card.name} />
+      <h3>Card Details</h3>
+        <p>{card.oracle_text}</p>
+      <h3>Mana Cost</h3>
+        <p>{card.mana_cost}</p>
+      <h3>Keywords</h3>
+        <p>{card.keywords.join(', ')}</p>
+      <h3>Set Information</h3>
+        <p>{card.set_name}</p>
+      <h3>Prices:</h3>
+        <ul>
+            <li>TCGPlayer (USD): ${card.prices.usd || 'N/A'}</li>
+            <li>Cardmarket (EUR): €{card.prices.eur || 'N/A'}</li>
+        </ul>
       <a href="/">Search another card</a>
     </div>
   );
